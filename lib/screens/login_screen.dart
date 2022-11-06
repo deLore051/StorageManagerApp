2df7fd82ad049f440e47c0bf01027ca835c46ffe
Stefan Dojcinovic <stefan.dojcinovic051@gmail.com>
2022-11-06 19:18:21 +0100

    update login_screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -14,15 +14,28 @@ class LoginScreenState extends State<LoginScreen> {
   final emailTextFieldController = TextEditingController();
   final passwordTextFieldController = TextEditingController();
 
+  bool _passwordVisible = true;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey.shade300,
-      body: SafeArea(
-        child: Center(
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      child: Center(
+        child: SingleChildScrollView(
           child: Column(
             children: [
-              SizedBox(height: 30),
+              Container(
+                padding: EdgeInsets.only(top: 10),
+                width: 300,
+                height: 300,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image:
+                            AssetImage("lib/resources/ingel_magacin_logo.png"),
+                        fit: BoxFit.contain)),
+              ),
               Text(
                 "Dobro došli!",
                 style: TextStyle(
@@ -30,17 +43,93 @@ class LoginScreenState extends State<LoginScreen> {
                   fontSize: 24,
                 ),
               ),
-              SizedBox(height: 20),
               Container(
+                padding: EdgeInsets.only(top: 10),
                 width: 300,
+                height: 70,
                 child: TextField(
+                  controller: emailTextFieldController,
+                  keyboardType: TextInputType.emailAddress,
+                  autocorrect: false,
+                  textCapitalization: TextCapitalization.none,
                   decoration: InputDecoration(
                       labelText: "Email",
-                      icon: Icon(Icons.email),
+                      suffixIcon: Icon(Icons.email_outlined),
                       border: OutlineInputBorder(
+                          borderSide: BorderSide(width: 2),
                           borderRadius: BorderRadius.circular(10))),
                 ),
               ),
+              Container(
+                padding: EdgeInsets.only(top: 10),
+                width: 300,
+                height: 70,
+                child: TextField(
+                  controller: passwordTextFieldController,
+                  autocorrect: false,
+                  textCapitalization: TextCapitalization.none,
+                  obscureText: _passwordVisible,
+                  decoration: InputDecoration(
+                      labelText: "Password",
+                      suffixIcon: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _passwordVisible = !_passwordVisible;
+                          });
+                        },
+                        child: _passwordVisible
+                            ? Icon(Icons.remove_red_eye_outlined)
+                            : Icon(Icons.remove_red_eye),
+                      ),
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(width: 2),
+                          borderRadius: BorderRadius.circular(10))),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 10),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(250, 50),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    "Uloguj se",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 10, bottom: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Nemate nalog?",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push<void>(
+                          context,
+                          MaterialPageRoute<void>(
+                              builder: (BuildContext context) =>
+                                  const SignUpScreen()),
+                        );
+                      },
+                      child: Text(
+                        " Registrujte se!",
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue),
+                      ),
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
